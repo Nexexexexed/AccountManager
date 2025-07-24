8afa567c5d047b586b5675404f4d2b8d6a03d4ed
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-07-24 11:41:04 +0300

    delete files and create Store Pinia

---
+++ src/stores/AccountStore.ts
@@ -0,0 +1,55 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+interface Label {
+  text: string;
+}
+
+interface Account {
+  id: number;
+  labels: Label[];
+  type: "LDAP" | "Локальная";
+  login: string;
+  password: string | null;
+}
+
+export const useAccountStore = defineStore("account", () => {
+  const accounts = ref<Account[]>(
+    JSON.parse(localStorage.getItem("accounts") || "[]")
+  );
+  const nextId = ref(
+    accounts.value.length > 0
+      ? Math.max(...accounts.value.map((a) => a.id)) + 1
+      : 1
+  );
+
+  function addAccount(): Account {
+    const newAccount: Account = {
+      id: nextId.value++,
+      labels: [],
+      type: "Локальная",
+      login: "",
+      password: "",
+    };
+    accounts.value.push(newAccount);
+    saveToLocalStorage();
+    return newAccount;
+  }
+
+  function updateAccount(updateAccout: Account): void {
+    const index = accounts.value.findIndex((a) => a.id === updateAccout.id);
+    if (index !== -1) {
+      accounts.value[index] = updateAccout;
+      saveToLocalStorage();
+    }
+  }
+
+  function deleteAccount(id: number): void {
+    accounts.value == accounts.value.filter((a) => a.id == id);
+    saveToLocalStorage();
+  }
+
+  function saveToLocalStorage() {
+    localStorage.setItem("accounts", JSON.stringify(accounts.value));
+  }
+});
