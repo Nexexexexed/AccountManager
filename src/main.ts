fa4ef7e69ddf96eab7d3d3b47daab8ac4a62fe75
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-07-24 13:22:52 +0300

    add UI Element-Plus

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(ElementPlus);
 
-app.use(createPinia())
-
-app.mount('#app')
+app.mount("#app");
